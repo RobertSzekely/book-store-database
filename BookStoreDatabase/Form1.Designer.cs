5474f18478540586a7dad5944abbd8aadc4c2d83
roobert95 <roobert95@yahoo.com>
2016-03-23 23:09:55 +0200

    fixed bug (Display Books > Display Publishers)
    if you pressed display books after display publishers (and vice-versa),
    you would get an error

--- BookStoreDatabase/Form1.Designer.cs
+++ BookStoreDatabase/Form1.Designer.cs
@@ -40,93 +40,111 @@
             this.addRecordButton = new System.Windows.Forms.Button();
             this.dg = new System.Windows.Forms.DataGridView();
             this.displayButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
             this.prevButton = new System.Windows.Forms.Button();
             this.nextButton = new System.Windows.Forms.Button();
             this.lastButton = new System.Windows.Forms.Button();
             this.firstButton = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
+            this.listBooks = new System.Windows.Forms.ListBox();
+            this.DisplayPub = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pubNameBox = new System.Windows.Forms.TextBox();
+            this.pubCityBox = new System.Windows.Forms.TextBox();
+            this.pubCountryBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 89);
+            this.label1.Location = new System.Drawing.Point(648, 477);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Book Title:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 127);
+            this.label2.Location = new System.Drawing.Point(648, 509);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(159, 25);
+            this.label2.Size = new System.Drawing.Size(129, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Publication Date:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(465, 88);
+            this.label3.Location = new System.Drawing.Point(648, 553);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 25);
+            this.label3.Size = new System.Drawing.Size(99, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Publisher ID:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(465, 131);
+            this.label4.Location = new System.Drawing.Point(648, 589);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 25);
+            this.label4.Size = new System.Drawing.Size(85, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Language:";
             // 
             // bookTitleBox
             // 
-            this.bookTitleBox.Location = new System.Drawing.Point(142, 85);
+            this.bookTitleBox.Location = new System.Drawing.Point(739, 474);
+            this.bookTitleBox.Margin = new System.Windows.Forms.Padding(2);
             this.bookTitleBox.Name = "bookTitleBox";
-            this.bookTitleBox.Size = new System.Drawing.Size(304, 29);
+            this.bookTitleBox.Size = new System.Drawing.Size(310, 26);
             this.bookTitleBox.TabIndex = 4;
             // 
             // pubDateBox
             // 
-            this.pubDateBox.Location = new System.Drawing.Point(197, 127);
+            this.pubDateBox.Location = new System.Drawing.Point(784, 509);
+            this.pubDateBox.Margin = new System.Windows.Forms.Padding(2);
             this.pubDateBox.Name = "pubDateBox";
-            this.pubDateBox.Size = new System.Drawing.Size(249, 29);
+            this.pubDateBox.Size = new System.Drawing.Size(265, 26);
             this.pubDateBox.TabIndex = 5;
             // 
             // publisherIDBox
             // 
-            this.publisherIDBox.Location = new System.Drawing.Point(594, 84);
+            this.publisherIDBox.Location = new System.Drawing.Point(754, 550);
+            this.publisherIDBox.Margin = new System.Windows.Forms.Padding(2);
             this.publisherIDBox.Name = "publisherIDBox";
-            this.publisherIDBox.Size = new System.Drawing.Size(134, 29);
+            this.publisherIDBox.Size = new System.Drawing.Size(295, 26);
             this.publisherIDBox.TabIndex = 6;
             // 
             // languageBox
             // 
-            this.languageBox.Location = new System.Drawing.Point(577, 128);
+            this.languageBox.Location = new System.Drawing.Point(740, 587);
+            this.languageBox.Margin = new System.Windows.Forms.Padding(2);
             this.languageBox.Name = "languageBox";
-            this.languageBox.Size = new System.Drawing.Size(151, 29);
+            this.languageBox.Size = new System.Drawing.Size(309, 26);
             this.languageBox.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(31, 42);
+            this.label5.Location = new System.Drawing.Point(648, 438);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(257, 25);
+            this.label5.Size = new System.Drawing.Size(210, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Add a book to the database:";
             // 
             // addRecordButton
             // 
-            this.addRecordButton.Location = new System.Drawing.Point(768, 67);
+            this.addRecordButton.Location = new System.Drawing.Point(652, 636);
+            this.addRecordButton.Margin = new System.Windows.Forms.Padding(2);
             this.addRecordButton.Name = "addRecordButton";
-            this.addRecordButton.Size = new System.Drawing.Size(139, 46);
+            this.addRecordButton.Size = new System.Drawing.Size(114, 54);
             this.addRecordButton.TabIndex = 9;
             this.addRecordButton.Text = "Add Record";
             this.addRecordButton.UseVisualStyleBackColor = true;
@@ -135,37 +153,31 @@
             // dg
             // 
             this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg.Location = new System.Drawing.Point(36, 188);
+            this.dg.Location = new System.Drawing.Point(29, 38);
+            this.dg.Margin = new System.Windows.Forms.Padding(2);
             this.dg.Name = "dg";
             this.dg.RowTemplate.Height = 31;
-            this.dg.Size = new System.Drawing.Size(1034, 629);
+            this.dg.Size = new System.Drawing.Size(582, 650);
             this.dg.TabIndex = 10;
+            this.dg.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellContentClick);
             // 
             // displayButton
             // 
-            this.displayButton.Location = new System.Drawing.Point(768, 120);
+            this.displayButton.Location = new System.Drawing.Point(917, 636);
+            this.displayButton.Margin = new System.Windows.Forms.Padding(2);
             this.displayButton.Name = "displayButton";
-            this.displayButton.Size = new System.Drawing.Size(139, 45);
+            this.displayButton.Size = new System.Drawing.Size(114, 54);
             this.displayButton.TabIndex = 11;
-            this.displayButton.Text = "Display";
+            this.displayButton.Text = "Display Books";
             this.displayButton.UseVisualStyleBackColor = true;
             this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
             // 
-            // cancelButton
-            // 
-            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(931, 848);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(139, 45);
-            this.cancelButton.TabIndex = 12;
-            this.cancelButton.Text = "Cancel";
-            this.cancelButton.UseVisualStyleBackColor = true;
-            // 
             // prevButton
             // 
-            this.prevButton.Location = new System.Drawing.Point(366, 847);
+            this.prevButton.Location = new System.Drawing.Point(199, 720);
+            this.prevButton.Margin = new System.Windows.Forms.Padding(2);
             this.prevButton.Name = "prevButton";
-            this.prevButton.Size = new System.Drawing.Size(148, 46);
+            this.prevButton.Size = new System.Drawing.Size(102, 38);
             this.prevButton.TabIndex = 13;
             this.prevButton.Text = "Previous";
             this.prevButton.UseVisualStyleBackColor = true;
@@ -173,9 +185,10 @@
             // 
             // nextButton
             // 
-            this.nextButton.Location = new System.Drawing.Point(520, 847);
+            this.nextButton.Location = new System.Drawing.Point(316, 720);
+            this.nextButton.Margin = new System.Windows.Forms.Padding(2);
             this.nextButton.Name = "nextButton";
-            this.nextButton.Size = new System.Drawing.Size(148, 46);
+            this.nextButton.Size = new System.Drawing.Size(109, 38);
             this.nextButton.TabIndex = 14;
             this.nextButton.Text = "Next";
             this.nextButton.UseVisualStyleBackColor = true;
@@ -183,9 +196,10 @@
             // 
             // lastButton
             // 
-            this.lastButton.Location = new System.Drawing.Point(674, 847);
+            this.lastButton.Location = new System.Drawing.Point(440, 720);
+            this.lastButton.Margin = new System.Windows.Forms.Padding(2);
             this.lastButton.Name = "lastButton";
-            this.lastButton.Size = new System.Drawing.Size(148, 46);
+            this.lastButton.Size = new System.Drawing.Size(118, 38);
             this.lastButton.TabIndex = 15;
             this.lastButton.Text = "Last";
             this.lastButton.UseVisualStyleBackColor = true;
@@ -193,9 +207,10 @@
             // 
             // firstButton
             // 
-            this.firstButton.Location = new System.Drawing.Point(212, 847);
+            this.firstButton.Location = new System.Drawing.Point(88, 720);
+            this.firstButton.Margin = new System.Windows.Forms.Padding(2);
             this.firstButton.Name = "firstButton";
-            this.firstButton.Size = new System.Drawing.Size(148, 46);
+            this.firstButton.Size = new System.Drawing.Size(96, 38);
             this.firstButton.TabIndex = 16;
             this.firstButton.Text = "First";
             this.firstButton.UseVisualStyleBackColor = true;
@@ -204,23 +219,110 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(31, 858);
+            this.label6.Location = new System.Drawing.Point(25, 690);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.Size = new System.Drawing.Size(0, 20);
             this.label6.TabIndex = 17;
+            // 
+            // listBooks
+            // 
+            this.listBooks.FormattingEnabled = true;
+            this.listBooks.ItemHeight = 20;
+            this.listBooks.Location = new System.Drawing.Point(652, 212);
+            this.listBooks.Margin = new System.Windows.Forms.Padding(2);
+            this.listBooks.Name = "listBooks";
+            this.listBooks.Size = new System.Drawing.Size(397, 224);
+            this.listBooks.TabIndex = 18;
+            // 
+            // DisplayPub
+            // 
+            this.DisplayPub.Location = new System.Drawing.Point(784, 636);
+            this.DisplayPub.Margin = new System.Windows.Forms.Padding(2);
+            this.DisplayPub.Name = "DisplayPub";
+            this.DisplayPub.Size = new System.Drawing.Size(114, 54);
+            this.DisplayPub.TabIndex = 19;
+            this.DisplayPub.Text = "Display Publishers";
+            this.DisplayPub.UseVisualStyleBackColor = true;
+            this.DisplayPub.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(652, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(239, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Add a publisher to the database:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(652, 80);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Name:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(652, 121);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 20);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "City:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(652, 163);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 20);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Country:";
+            // 
+            // pubNameBox
+            // 
+            this.pubNameBox.Location = new System.Drawing.Point(714, 80);
+            this.pubNameBox.Name = "pubNameBox";
+            this.pubNameBox.Size = new System.Drawing.Size(335, 26);
+            this.pubNameBox.TabIndex = 24;
+            // 
+            // pubCityBox
+            // 
+            this.pubCityBox.Location = new System.Drawing.Point(714, 118);
+            this.pubCityBox.Name = "pubCityBox";
+            this.pubCityBox.Size = new System.Drawing.Size(335, 26);
+            this.pubCityBox.TabIndex = 25;
+            // 
+            // pubCountryBox
+            // 
+            this.pubCountryBox.Location = new System.Drawing.Point(726, 160);
+            this.pubCountryBox.Name = "pubCountryBox";
+            this.pubCountryBox.Size = new System.Drawing.Size(323, 26);
+            this.pubCountryBox.TabIndex = 26;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(1102, 923);
+            this.ClientSize = new System.Drawing.Size(1106, 795);
+            this.Controls.Add(this.pubCountryBox);
+            this.Controls.Add(this.pubCityBox);
+            this.Controls.Add(this.pubNameBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DisplayPub);
+            this.Controls.Add(this.listBooks);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.firstButton);
             this.Controls.Add(this.lastButton);
             this.Controls.Add(this.nextButton);
             this.Controls.Add(this.prevButton);
-            this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.displayButton);
             this.Controls.Add(this.dg);
             this.Controls.Add(this.addRecordButton);
@@ -234,6 +336,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -257,12 +360,20 @@
         private System.Windows.Forms.Button addRecordButton;
         private System.Windows.Forms.DataGridView dg;
         private System.Windows.Forms.Button displayButton;
-        private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button prevButton;
         private System.Windows.Forms.Button nextButton;
         private System.Windows.Forms.Button lastButton;
         private System.Windows.Forms.Button firstButton;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBooks;
+        private System.Windows.Forms.Button DisplayPub;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox pubNameBox;
+        private System.Windows.Forms.TextBox pubCityBox;
+        private System.Windows.Forms.TextBox pubCountryBox;
     }
 }
 
--- BookStoreDatabase/Form1.cs
+++ BookStoreDatabase/Form1.cs
@@ -13,11 +13,12 @@ namespace BookStoreDatabase
 {
     public partial class MainWindow : Form
     {
-        DataSet ds = new DataSet();
-        SqlConnection cs = new SqlConnection("Data Source = DESKTOP-3JU4JU4; Initial Catalog = BookStore; Integrated Security = TRUE");
-        SqlDataAdapter da = new SqlDataAdapter();
+        private DataSet ds = new DataSet();
+        private SqlConnection cs = new SqlConnection("Data Source = DESKTOP-3JU4JU4; Initial Catalog = BookStore; Integrated Security = TRUE");
+        private SqlDataAdapter da = new SqlDataAdapter();
 
-        BindingSource tblBooksBS = new BindingSource();
+        private BindingSource tblBooksBS = new BindingSource();
+        private BindingSource tblPublishersBS = new BindingSource();
 
         public MainWindow()
         {
@@ -27,12 +28,15 @@ namespace BookStoreDatabase
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dg.DataSource = tblBooksBS;
+            dg.AutoGenerateColumns = true;
+          
 
         }
 
         private void addRecordButton_Click(object sender, EventArgs e)
         {
-            
+            //add a record to the Book table   
             da.InsertCommand = new SqlCommand("INSERT INTO Books VALUES (@book_title, @pub_date, @pud_id, @language)", cs);
             da.InsertCommand.Parameters.Add("@book_title", SqlDbType.VarChar).Value = bookTitleBox.Text;
             da.InsertCommand.Parameters.Add("@pub_date", SqlDbType.Date).Value = pubDateBox.Text;
@@ -46,7 +50,8 @@ namespace BookStoreDatabase
 
         private void displayButton_Click(object sender, EventArgs e)
         {
-            da.SelectCommand = new SqlCommand("SELECT * FROM Books", cs);
+            //display all the records from Book table
+            da.SelectCommand = new SqlCommand("SELECT book_title, pub_date, publisher_id, language FROM Books", cs);
             ds.Clear();
             //da.Fill(ds);
             da.Fill(ds, "Books");
@@ -54,51 +59,119 @@ namespace BookStoreDatabase
 
             tblBooksBS.DataSource = ds.Tables["Books"];
 
-            bookTitleBox.DataBindings.Add(new Binding("Text", tblBooksBS, "book_title"));
-            pubDateBox.DataBindings.Add(new Binding("Text", tblBooksBS, "pub_date"));
-            publisherIDBox.DataBindings.Add(new Binding("Text", tblBooksBS, "publisher_id"));
-            languageBox.DataBindings.Add(new Binding("Text", tblBooksBS, "language"));
+            records();
+
+            //disable the buttons for navigating
+            nextButton.Enabled = false;
+            prevButton.Enabled = false;
+            firstButton.Enabled = false;
+            lastButton.Enabled = false;
+
+           // da.SelectCommand = new SqlCommand("SELECT book_title From Books", cs);
+            //da.Fill(ds, "Books");
+            //listBooks.DataSource = ds.Tables["Books"];
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //enable the navigation buttons
+            nextButton.Enabled = true;
+            prevButton.Enabled = true;
+            firstButton.Enabled = true;
+            lastButton.Enabled = true;
+
+            //display all the records from the Publishers table
+            //ds.Clear();
+            da.SelectCommand = new SqlCommand("SELECT publisher_name, city, country FROM Publishers", cs);
+            ds.Clear();
+            da.Fill(ds);
+            da.Fill(ds, "Publishers");
+            dg.DataSource = ds.Tables["Publishers"];
+
+            tblPublishersBS.DataSource = ds.Tables["Publishers"];
+
             records();
         }
 
         private void nextButton_Click(object sender, EventArgs e)
         {
-            tblBooksBS.MoveNext();
-            dgUpdate();
+            //tblBooksBS.MoveNext();
+            tblPublishersBS.MoveNext();
+            dgUpdatePublishers();
             records();
         }
 
         private void prevButton_Click(object sender, EventArgs e)
         {
-            tblBooksBS.MovePrevious();
-            dgUpdate();
+            //tblBooksBS.MovePrevious();
+            tblPublishersBS.MovePrevious();
+            dgUpdatePublishers();
             records();
         }
 
         private void lastButton_Click(object sender, EventArgs e)
         {
-            tblBooksBS.MoveLast();
-            dgUpdate();
+            //tblBooksBS.MoveLast();
+            tblPublishersBS.MoveLast();
+            dgUpdatePublishers();
             records();
         }
 
         private void firstButton_Click(object sender, EventArgs e)
         {
-            tblBooksBS.MoveFirst();
-            dgUpdate();
+            //tblBooksBS.MoveFirst();
+            tblPublishersBS.MoveFirst();
+            dgUpdatePublishers();
             records();
             
         }
 
-        private void dgUpdate()
+        private void dgUpdatePublishers()
         {
-            dg.ClearSelection();
-            dg.Rows[tblBooksBS.Position].Selected = true;
+            dg.ClearSelection();//clear datagrid selection
+            //dg.Rows[tblBooksBS.Position].Selected = true;
+            dg.Rows[tblPublishersBS.Position].Selected = true;
+
+            pubNameBox.DataBindings.Add(new Binding("Text", tblPublishersBS, "publisher_name"));
+            pubCityBox.DataBindings.Add(new Binding("Text", tblPublishersBS, "city"));
+            pubCountryBox.DataBindings.Add(new Binding("Text", tblPublishersBS, "country"));
+
+            pubNameBox.DataBindings.Clear();
+            pubCityBox.DataBindings.Clear();
+            pubCountryBox.DataBindings.Clear();
+
+
             records();
+        }
+        private void dgUpdateBooks()
+        {
+            dg.ClearSelection();//clear datagrid selection
+            dg.Rows[tblBooksBS.Position].Selected = true;
+
+            bookTitleBox.DataBindings.Add(new Binding("Text", tblBooksBS, "book_title"));
+            pubDateBox.DataBindings.Add(new Binding("Text", tblBooksBS, "pub_date"));
+            publisherIDBox.DataBindings.Add(new Binding("Text", tblBooksBS, "publisher_id"));
+            languageBox.DataBindings.Add(new Binding("Text", tblBooksBS, "language"));
+
+            bookTitleBox.DataBindings.Clear();
+            pubDateBox.DataBindings.Clear();
+            publisherIDBox.DataBindings.Clear();
+            languageBox.DataBindings.Clear();
+
         }
         private void records()
         {
             label6.Text = "Record" + tblBooksBS.Position + " of " + (tblBooksBS.Count - 1);
+        }
+
+        private void dg_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
